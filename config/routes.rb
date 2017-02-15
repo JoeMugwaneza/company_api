cc8a0568391b82b721d4e38193ccbc5949741eb7
JoeMugwaneza <muganezajoseph@gmail.com>
2017-02-15 11:12:18 +0200

    worked on api versioning

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,14 @@ Rails.application.routes.draw do
   post "/companies", to: 'companies#create'
   patch "/companies/:id", to: 'companies#update'
   delete "/companies/:id", to: 'companies#destroy'
+
+  namespace :api do
+    namespace :v1 do 
+      get "/companies", to: 'companies#index'
+      post "/companies", to: 'companies#create'
+      patch "/companies/:id", to: 'companies#update'
+      delete "/companies/:id", to: 'companies#destroy'
+    end 
+  end 
+
 end
